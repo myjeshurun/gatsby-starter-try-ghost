f202f02f099145adae8bd10af33a121dcf65fc7a
myjeshurun <qingxin@gmail.com>
2020-12-28 19:02:56 +0800

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -10,13 +10,13 @@ module.exports = {
     postsPerPage: 3,
 
     // This allows an alternative site title for meta data for pages.
-    siteTitleMeta: `Gatsby Starter Ghost CMS`,
+    siteTitleMeta: `寻求你面`,
 
     // This allows an site description for meta data for pages.
     siteDescriptionMeta: `Turn your Ghost blog into a flaring fast static site with Gatsby`,
 
     // Used for App and Offline manifest e.g. Mobile Home Screen
-    shortTitle: `Jamify`,
+    shortTitle: `寻求你面`,
     siteIcon: `favicon.png`,
     backgroundColor: `#e9e9e9`,
     themeColor: `#15171A`,
